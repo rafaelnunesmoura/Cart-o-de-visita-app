599f7eebc78deee9e0ec902a937c7c816abf4a8e
rafael <rafaelnunesmoura@protonmail.com>
2022-03-14 15:42:04 -0300

    Project structure

---
+++ app/src/main/java/com/example/cartaodevisitas/ui/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.cartaodevisitas.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.cartaodevisitas.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
